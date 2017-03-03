13cacb5959c90cd2579f1e01df99f9c8a8f4f813
DerekStride <djgstride@gmail.com>
2017-03-02 20:01:16 -0500

    Setup routes and autoload paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/graphql', to: 'graphql#create'
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: '/graphql'
 end
